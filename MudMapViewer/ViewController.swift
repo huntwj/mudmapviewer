97d412ae8d84a58952c3b2bf61626ba918ec0b91
Wil Hunt <huntwj@gmail.com>
2015-11-13 18:55:14 -0800

    Initial Commit

---
+++ MudMapViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MudMapViewer
+//
+//  Created by Wil Hunt on 11/13/15.
+//  Copyright © 2015 William Hunt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
